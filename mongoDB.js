780e14908023b2fe65a03f43083df4da2af3d41d
Arya <arya12345kishan@gmail.com>
2024-02-04 15:35:10 +0530

    mongoChange

--- mongoDB.js
+++ mongoDB.js
@@ -1,21 +1,6 @@
-// const mongoose = require('mongoose')
-
-// mongoose.connect(process.env.MONGO_URL)
-//     .then(() => {
-//         console.log("CONNECTED TO MONGO DB");
-//     })
-//     .catch((err) => {
-//         console.log("ERROR IN CONNECTING MONGO DB");
-//         console.log(err);
-//     })
-
-
-
-
-
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/my-mern-ecommerce')
+mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log("CONNECTED TO MONGO DB");
     })
@@ -23,3 +8,18 @@ mongoose.connect('mongodb://localhost:27017/my-mern-ecommerce')
         console.log("ERROR IN CONNECTING MONGO DB");
         console.log(err);
     })
+
+
+
+
+
+// const mongoose = require('mongoose')
+
+// mongoose.connect('mongodb://localhost:27017/my-mern-ecommerce')
+//     .then(() => {
+//         console.log("CONNECTED TO MONGO DB");
+//     })
+//     .catch((err) => {
+//         console.log("ERROR IN CONNECTING MONGO DB");
+//         console.log(err);
+//     })
